b559443a2629c06c8fda377aa80fd2907154484a
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-04-13 10:29:00 -0300

    somar

---
+++ java-basico/java-basico/src/Somar.java
@@ -0,0 +1,11 @@
+// aprendendo algumas palavras reservadas na prática
+public class Somar {
+
+
+    public static void main(String[] args) {
+        System.out.println(somar(432, 22));
+    }
+    public static int somar(int n1, int n2){
+        return n1 + n2;
+    }
+}
