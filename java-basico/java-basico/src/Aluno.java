4d4f63af7393a74ff51df158867225991964e938
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-04-13 14:19:01 -0300

    alunos em java

---
+++ java-basico/java-basico/src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno {
+    String nome;
+    int idade;
+}
+
+class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.nome="Felipe";
+        felipe.idade = 8;
+
+        System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
+    }
+}
