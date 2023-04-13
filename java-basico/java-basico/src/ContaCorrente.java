0d021b068804339bbde35e296765871af366e0c3
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-04-13 10:04:51 -0300

    alteração

--- java-basico/java-basico/src/ContaCorrente.java
+++ java-basico/java-basico/src/ContaCorrente.java
@@ -1,16 +1,22 @@
 public class ContaCorrente {
-        int numeroConta = 1;
-        int numeroAgencia = 50;
-        double saldoConta = 100.50;
-        String nomeCliente = "Carlos Henrique";
-        String dataNascimento = "16/04/2006";
+    int numeroConta = 1;
+    int numeroAgencia = 50;
+    double saldoConta = 100.50;
+    String nomeCliente = "Carlos Henrique";
+    String dataNascimento = "16/04/2006";
 
     public static void main(String[] args) {
         ContaCorrente minhaConta = new ContaCorrente();
         minhaConta.sacar(14.40);
+        minhaConta.depositar(20.39);
         System.out.println("O valor atual da conta é: " + minhaConta.saldoConta);
     }
+
     public void sacar(double valorSaque) {
         saldoConta = saldoConta - valorSaque;
+    }
+
+    public void depositar(double valorDeposito) {
+        saldoConta += valorDeposito;
     }
 }
