3db0c38f81bc0fa3d07764fffc04d3c0cb30512c
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-04-14 10:00:26 -0300

    hora do sistema em java

--- java-basico/java-basico/src/HoraDoSistema.java
+++ java-basico/java-basico/src/HoraDoSistema.java
@@ -3,7 +3,7 @@ public class HoraDoSistema {
 
     public static void main(String[] args) {
         Date relogio = new Date(); // criando um objeto data
-        System.out.println("O relogio do sistema é:");
+        System.out.println("O relógio do sistema é:");
         System.out.println(relogio.toString());
     }
 }
